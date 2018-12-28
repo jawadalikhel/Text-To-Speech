c4cfae8a6f3bc695fce2de92cfde2c9451a064c2
Your Name <jawadalikhel18@gmail.com>
2018-12-27 22:48:47 -0600

    all of the languages and name of the lang is being display at the option on screen

--- TYPE_N_SPEECH/dist/js/main.js
+++ TYPE_N_SPEECH/dist/js/main.js
@@ -11,3 +11,32 @@ const rate = document.querySelector('#rate');
 const rateValue = document.querySelector('#rate-value');
 const pitch = document.querySelector('#pitch');
 const pitchValue = document.querySelector('#pitch-value');
+
+// Init voices array
+let voices = [];
+
+const getVoices = () => {
+  // synth.getVoices is the api for all of the voices 
+  voices = synth.getVoices();
+
+  // loop through voices and create an option for earch one
+  voices.forEach(voice =>{
+    // create option Element
+    const option = document.createElement('option');
+    // fill option with voice and language
+    option.textContent = voice.name + '('+ voice.lang +')';
+
+    // set needed option attributes
+    option.setAttribute('data-lang', voice.lang);
+    option.setAttribute('data-name', voice.name);
+    voiceSelect.appendChild(option);
+
+  })
+  console.log(voices);
+};
+
+getVoices();
+
+if(synth.onvoiceschanged !== undefined){
+  synth.onvoiceschanged = getVoices;
+}
